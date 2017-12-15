2343b58734ba5489b0904654cba0c294c8be13c8
kmenghini <kaitlyn.menghini@gmail.com>
2017-12-14 19:14:09 -0800

    update CSS

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -6,12 +6,12 @@ var VideoPlayer = (props) => {
       </div>
       <div className="video-player-details">
         <h3>{props.state.currentVideo.snippet.title}</h3>
-        <div id="views">View Count: {props.state.currentVideo.statistics.viewCount}</div>
-        <div id="dislikes">Number of Dislikes: {props.state.currentVideo.statistics.dislikeCount}</div>
-        <div id="likes">Number of Likes: {props.state.currentVideo.statistics.likeCount}</div>
-        <div>Channel: {props.state.currentVideo.snippet.channelTitle}</div>
-        <div>Published on {moment(props.state.currentVideo.snippet.publishedAt).format('MMMM Do, YYYY')}</div>
-        <div>{props.state.currentVideo.snippet.description}</div>
+        <div class="video-detail-bar" id="dislikes">{Number(props.state.currentVideo.statistics.dislikeCount).toLocaleString()} dislikes</div>
+        <div class="video-detail-bar" id="likes">{Number(props.state.currentVideo.statistics.likeCount).toLocaleString()} likes</div>
+        <div class="video-detail-bar" id="views">{Number(props.state.currentVideo.statistics.viewCount).toLocaleString()} views</div>
+        <div id="channel-title">{props.state.currentVideo.snippet.channelTitle}</div>
+        <div id="date-published">Published on {moment(props.state.currentVideo.snippet.publishedAt).format('MMMM Do, YYYY')}</div>
+        <div id="description-box">{props.state.currentVideo.snippet.description}</div>
       </div>
     </div>
   );
